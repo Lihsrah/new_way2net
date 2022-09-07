435778c45851b391aa0a3e0a6d84cd64be9abe98
fkin <fkin@black.black>
2022-09-07 14:03:42 -0400

    ankit again

---
+++ app.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const routes = require('./routes')
+const port = process.env.PORT || 8008
+const cors = require('cors')
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+const path = require('path')
+const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(cors())
+app.use(routes)
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/views'))
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+app.set('views', __dirname + '/views');
+
+
+
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname , 'cert','key.pem')),
+  cert: fs.readFileSync(path.join(__dirname , 'cert','cert.pem')),
+}, app);
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+
---
+++ routes.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require("express")
+const router = express.Router()
+
+router.get('/', async(req,res)=>{
+    try {
+        res.render('index')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+module.exports = router
